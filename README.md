1c31a540d513b11bd3f130af8808bf793d87a1a1
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2024-05-22 19:05:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# DizzyMo_
+
+## Requirements
+
+[Node.js](https://nodejs.org) is required to install dependencies and run scripts via `npm`.
+
+## Available Commands
+
+| Command | Description |
+|---------|-------------|
+| `npm install` | Install project dependencies |
+| `npm run dev` | Launch a development web server |
+| `npm run build` | Create a production build in the `dist` folder |
+
+## Setup
+
+After cloning the repo, run `npm install` from your project directory. Then, you can start the local development server by running `npm run dev`.
+
+The local development server runs on `http://localhost:8080` by default. Please see the Vite documentation if you wish to change this, or add SSL support.
+
+All rights reserved.
---
+++ src/game/scenes/Game.js
@@ -0,0 +1,96 @@
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
+
+var platforms, player, cursors, stars;
+
+export class Game extends Scene {
+    constructor() {
+        super("Game");
+    }
+
+    create() {
+        this.add.image(512, 384, "background").setScale(3);
+        platforms = this.physics.add.staticGroup();
+        platforms.create(400, 568, "ground").setScale(2).refreshBody();
+        platforms.create(600, 400, "ground");
+        platforms.create(50, 250, "ground");
+        platforms.create(750, 220, "ground");
+
+        player = this.physics.add.sprite(100, 450, "dude");
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+        player.body.setGravityY(50);
+
+        cursors = this.input.keyboard.createCursorKeys();
+
+        stars = this.physics.add.group({
+            key: "star",
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 },
+        });
+
+        stars.children.iterate(function (child) {
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+
+        this.physics.add.collider(player, platforms);
+        this.physics.add.collider(stars, platforms);
+        this.physics.add.overlap(player, stars, collectStar, null, this);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        function collectStar(player, star) {
+            star.disableBody(true, true);
+        }
+
+        EventBus.emit("current-scene-ready", this);
+    }
+
+    update() {
+        if (cursors.left.isDown) {
+            player.setVelocityX(-160);
+
+            player.anims.play("left", true);
+        } else if (cursors.right.isDown) {
+            player.setVelocityX(160);
+
+            player.anims.play("right", true);
+        } else {
+            player.setVelocityX(0);
+
+            player.anims.play("turn");
+        }
+
+        if (cursors.up.isDown && player.body.touching.down) {
+            player.setVelocityY(-330);
+        }
+    }
+
+    changeScene() {
+        this.scene.start("GameOver");
+    }
+}
